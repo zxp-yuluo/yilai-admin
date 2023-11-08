d43e0f864fea7da7ecfe393850d400cf78119014
zxp-yuluo <zxp_10012991@163.com>
2023-11-08 12:51:10 +0800

    引入element-ui

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <el-row>
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+  </div>
+</template>
+
+<style scoped></style>
